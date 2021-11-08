4f3c35e3a1414f5b6aeb5b6b7f8ad0faa1df4c35
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2021-11-08 18:53:28 +0000

    Update devcontainer to include gcloud SDK and Skaffold and fix permissions issue with minikube

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -7,5 +7,8 @@ wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O 
  
 # initialize dapr
 dapr init
- 
+
+# Fix minikube permissions
+sudo chown -R $USER $HOME/.minikube; chmod -R u+wrx $HOME/.minikube
+
 echo "on-create complete" >> ~/status
