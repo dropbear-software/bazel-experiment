01399d19a2bede18fef76ba20959d7f9037ab6d6
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2021-11-09 00:33:43 +0000

    Add Flutter and protoc to devcontainer

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -11,4 +11,19 @@ dapr init
 # Fix minikube permissions
 sudo chown -R $USER $HOME/.minikube; chmod -R u+wrx $HOME/.minikube
 
+# Install Protobuf compiler
+PROTOC_VERSION="3.19.0"
+curl -o /tmp/protoc.zip -L https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-linux-x86_64.zip
+RUN unzip /tmp/protoc.zip -d /home/${USERNAME}/.local/
+RUN rm /tmp/protoc.zip
+
+# Install Flutter
+git clone https://github.com/flutter/flutter.git -b stable ~/.flutter
+
+~/.flutter/bin/flutter precache --web
+
+# Get Relevant Dart CLIs needed for development
+dart pub global activate protoc_plugin && dart pub global activate mono_repo
+
+
 echo "on-create complete" >> ~/status
