60de7b296c1c498fe705101e6a445bf4b6027c7e
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2021-11-09 01:03:40 +0000

    Few more small tweaks to dev container.

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -4,26 +4,22 @@ echo "on-create start" >> ~/status
  
 # install dapr cli
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
- 
-# initialize dapr
-dapr init
 
 # Fix minikube permissions
 sudo chown -R $USER $HOME/.minikube; chmod -R u+wrx $HOME/.minikube
 
 # Install Protobuf compiler
-PROTOC_VERSION="3.19.0"
+PROTOC_VERSION="3.19.1"
 curl -o /tmp/protoc.zip -L https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-linux-x86_64.zip
-RUN unzip /tmp/protoc.zip -d /home/${USERNAME}/.local/
-RUN rm /tmp/protoc.zip
+unzip /tmp/protoc.zip -d /home/${USER}/.local/
+rm /tmp/protoc.zip
 
 # Install Flutter
 git clone https://github.com/flutter/flutter.git -b stable ~/.flutter
 
-~/.flutter/bin/flutter precache --web
+flutter precache --web
 
 # Get Relevant Dart CLIs needed for development
 dart pub global activate protoc_plugin && dart pub global activate mono_repo
-
 
 echo "on-create complete" >> ~/status
