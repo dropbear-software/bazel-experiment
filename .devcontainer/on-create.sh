cf8f8805081c15878e5ba2e52767e2f9c8019b52
mark-dropbear <76698672+mark-dropbear@users.noreply.github.com>
2021-11-20 14:34:19 +0000

    Simplify devcontainer setup. kubectl, minikube, skaffold and gcloud are now all managed via the Cloud Code plugin.

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -5,9 +5,6 @@ echo "on-create start" >> ~/status
 # install dapr cli
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
 
-# Fix minikube permissions
-sudo chown -R $USER $HOME/.minikube; chmod -R u+wrx $HOME/.minikube
-
 # Install Protobuf compiler
 PROTOC_VERSION="3.19.1"
 curl -o /tmp/protoc.zip -L https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOC_VERSION}/protoc-${PROTOC_VERSION}-linux-x86_64.zip
